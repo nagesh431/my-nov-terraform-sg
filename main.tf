b4a5da2fec7e7fe983afdf38e07b203153e3bb42
nagesh <nageshwarraomalothu@gmail.com>
2026-01-07 21:10:39 +0530

    added-2

--- main.tf
+++ main.tf
@@ -14,3 +14,18 @@ resource "aws_vpc_security_group_ingress_rule" "myingressrules" {
     cidr_ipv4 = "0.0.0.0/0"
   
 }
+resource "aws_key_pair" "myownkey" {
+    public_key = file("~/.ssh/id_ed25519.pub")  
+    key_name = "mysshownkey"
+}
+resource "aws_instance" "myec2" {
+    ami = "ami-02b8269d5e85954ef"
+    instance_type = "t.micro"
+    key_name = aws_key_pair.myownkey.key_name
+    associate_public_ip_address = true
+    vpc_security_group_ids = [aws_security_group.myownsg.id]
+    tags = {
+        Name = "myterraformec2"
+    }
+  
+}
